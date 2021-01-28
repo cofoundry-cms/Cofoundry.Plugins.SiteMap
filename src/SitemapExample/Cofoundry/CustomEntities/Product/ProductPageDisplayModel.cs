b40286d4541dab7b066649c68c04489413e18e67
Joel Mitchell <2780344+HeyJoel@users.noreply.github.com>
2021-01-28 12:11:49 +0000

    Updated to CF 0.9

---
+++ src/SitemapExample/Cofoundry/CustomEntities/Product/ProductPageDisplayModel.cs
@@ -0,0 +1,17 @@
+﻿using Cofoundry.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SitemapExample
+{
+    public class ProductPageDisplayModel : ICustomEntityPageDisplayModel<ProductDataModel>
+    {
+        public string PageTitle { get; set; }
+
+        public string MetaDescription { get; set; }
+
+        public string ShortDescription { get; set; }
+    }
+}
---
+++ src/SitemapExample/Cofoundry/CustomEntities/Product/ProductPageDisplayModelMapper.cs
@@ -0,0 +1,27 @@
+﻿using Cofoundry.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SitemapExample
+{
+    public class ProductPageDisplayModelMapper : ICustomEntityDisplayModelMapper<ProductDataModel, ProductPageDisplayModel>
+    {
+        public Task<ProductPageDisplayModel> MapDisplayModelAsync(
+            CustomEntityRenderDetails renderDetails, 
+            ProductDataModel dataModel, 
+            PublishStatusQuery publishStatusQuery
+            )
+        {
+            var displayModel = new ProductPageDisplayModel()
+            {
+                PageTitle = renderDetails.Title,
+                MetaDescription = dataModel.ShortDescription,
+                ShortDescription = dataModel.ShortDescription
+            };
+
+            return Task.FromResult(displayModel);
+        }
+    }
+}
